0241595802fac2a6fd634273108eaa166bc597bf
Nihil Hesara <nihilhesa@gmail.com>
2024-10-12 10:46:45 +0530

    for loop end update

--- Python/python04.py
+++ Python/python04.py
@@ -1,8 +1,11 @@
 for i  in range(5): # Ending value
-    print(i)
+    print(i, end=" ")
+print("\n")
 
 for i in range(1,6): # Starting and Ending
-    print(i)
+    print(i, end=" ")
+print("\n")
 
 for i in range(1,11,2): # Starting, Ending and Increment
-    print(i)
+    print(i, end=" ")
+print("\n")
