f36f9f431c4269610e099417144e52d9f2f95138
Nihil Hesara <nihilhesa@gmail.com>
2024-10-12 10:37:31 +0530

    for loop

---
+++ Python/python02.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number : "))
+
+if num % 3 == 0:
+    print("It's divisable")
+else:
+    print("It's not divisable")
---
+++ Python/python04.py
@@ -0,0 +1,8 @@
+for i  in range(5): # Ending value
+    print(i)
+
+for i in range(1,6): # Starting and Ending
+    print(i)
+
+for i in range(1,11,2): # Starting, Ending and Increment
+    print(i)
