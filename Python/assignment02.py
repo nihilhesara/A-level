dbd4d7c6bee3517447206f4525e1e522fc772783
Nihil Hesara <nihilhesa@gmail.com>
2024-11-09 10:01:17 +0530

    Add asignment - 02

--- Python/assignment02.py
+++ Python/assignment02.py
@@ -1,50 +1,11 @@
-# Creating functions for operators
-def add(num1,num2):
-    return num1 + num2
+def check(num):
+    if num == 1:
+        print("True")
+    elif num == 0:
+        print("False")
+    else:
+        print("Not a valid input")
 
-def sub(num1,num2):
-    return num1 - num2
+num = int(input("Enter 0 or 1 to execute the program : "))
 
-def mul(num1,num2):
-    return num1 * num2
-
-def div(num1,num2):
-    return num1 / num2
-
-def mod(num1,num2):
-    return num1 % num2
-
-def power(num1,num2):
-    return num1 ** num2
-
-#__________________________________________________________________
-# MAIN PROGRAM
-
-# Getting inputs
-operator = input("Enter a operator : ")
-print("")
-number1 = int(input("Enter number : "))
-number2 = int(input("Enter number : "))
-
-# Calling the function using nested if 
-if operator == '+':
-    print(f"\nAddition is = {add(number1,number2)}")
-
-elif operator == '-':
-    print(f"\nSubstraction is = {sub(number1,number2)}")
-
-elif operator == '*':
-    print(f"\nMultiplication is = {mul(number1,number2)}")
-
-elif operator == '/':
-    print(f"\nDivision is = {div(number1,number2)}")
-
-elif operator == '%':
-    print(f"Modules is = {mod(number1,number2)}")
-
-elif operator == '**':
-    print(f"Power is = {power(number1,number2)}")
-
-else:
-    print("Invalid operator!!!")
-
+check(num)
