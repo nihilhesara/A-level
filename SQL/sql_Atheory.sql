c94e35c5837534b6789cb841db39bc79aa9c623d
Nihil Hesara <nihilhesa@gmail.com>
2024-11-09 13:43:27 +0530

    Add 3 tables to Atheory

--- SQL/sql_Atheory.sql
+++ SQL/sql_Atheory.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE school;
 
+--__________________________________________________________
+
 CREATE TABLE student
 (
     StuId VARCHAR(5) NOT NULL,
@@ -13,3 +15,56 @@ INSERT INTO student
 VALUES
 ('ST001','Hesara','Rajagiriya'),
 ('ST002','Nimadith','Borella');
+
+--__________________________________________________________
+
+CREATE TABLE course
+(
+    CourseId VARCHAR(4) NOT NULL,
+    CourseName VARCHAR(10),
+    PRIMARY KEY (CourseId)
+);
+
+INSERT INTO course
+(CourseId, CourseName)
+VALUES
+('C01','Python'),
+('C02','C++'),
+('C03','SDLC');
+
+--__________________________________________________________
+
+CREATE TABLE city
+(
+    City VARCHAR(12) NOT NULL,
+    PostCode VARCHAR(5),
+    PRIMARY KEY (City)
+);
+
+INSERT INTO city
+(City, PostCode)
+VALUES
+('Rajagiriya', '10100'),
+('Borella', '00800');
+
+--__________________________________________________________
+
+CREATE TABLE marks
+(
+    StuId VARCHAR(5) NOT NULL,
+    CourseId VARCHAR(4) NOT NULL,
+    Marks INT(3),
+    PRIMARY KEY (StuId, CourseId),
+    FOREIGN KEY (StuId) REFERENCES student(StuId),
+    FOREIGN KEY (CourseId) REFERENCES course(CourseId)
+);
+
+
+INSERT INTO marks
+(StuId, CourseId, Marks)
+VALUES
+('ST001','C01',78),
+('ST001','C03',88), 
+('ST002','C01',60), 
+('ST002','C02',70),  
+('ST002','C03',75) 
